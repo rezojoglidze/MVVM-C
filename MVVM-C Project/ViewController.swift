72b503b138fd70491b4a5bfec13f2bdc2982ecda
Rezo Joglidze <rezojoglidze7@gmail.com>
2022-02-23 23:44:47 +0400

    Initial Commit

---
+++ MVVM-C Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C Project
+//
+//  Created by Rezo Joglidze on 23.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
